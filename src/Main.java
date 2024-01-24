0ce26c1b7926a1d5118f9ae307fc47a58c0f2fda
Magdalenacze <126948179+Magdalenacze@users.noreply.github.com>
2024-01-24 13:04:28 +0100

    Project_1-TicTacToe
    Commit and push changes done by GitHub Desktop application:
    
    1. The basic form of the tic-tac-toe game.
    2. First modification: added the ability to define the board size.
    3. Second modification: added the ability to start a new game or end the game.
    4. Third modification: added requirement to enter players' names/nicknames before starting the game, and a summary after the game, including statistics.
    
    I think that "in the future" I could try to optimize the code using lambda expressions for collections, loops and if-else conditional statements.
    
    Moreover, I think that displaying the game summary could also be solved using an enum and an enum loop (useful in games with a large number of players at the same time, and in our case there are only X and 0).

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,84 @@
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-
-        char[][] board = new char[3][3];
-        char activePlayer = 'X';
-        int i;
-        int j;
-
-        completeTheBoard(board);
-        printTheBoard(board);
 
         try (Scanner scanner = new Scanner(System.in)) {
 
-            while (gameResult(board) == '1') {
+            String continueOrGameOver;
+            String playerName;
+            int numberOfBoardsWon = 0;
 
-                System.out.println("Player " + activePlayer + ", give both coordinates: ");
-                i = scanner.nextInt();
-                j = scanner.nextInt();
+            Map<Character, String> playersMap = new HashMap<>();
+            Map<String, Integer> mapOfWinningBoards = new HashMap<>();
 
-                while (checkCoordinates(board, i, j)) {
-                    System.out.println("Values too big. Enter max value " + (board.length - 1) + ": ");
-                    i = scanner.nextInt();
-                    j = scanner.nextInt();
-                }
+            System.out.println("Please enter the name of the first player(as player X): ");
+            playerName = scanner.nextLine();
+            playersMap.put('X', playerName);
+            mapOfWinningBoards.put(playerName, numberOfBoardsWon);
 
-                while (!updateTheBoard(board, i, j, activePlayer)) {
-                    System.out.println("Field occupied. Enter other coordinates: ");
-                    i = scanner.nextInt();
-                    j = scanner.nextInt();
-                }
+            System.out.println("Please enter the name of the second player(as player 0): ");
+            playerName = scanner.nextLine();
+            playersMap.put('0', playerName);
+            mapOfWinningBoards.put(playerName, numberOfBoardsWon);
 
+            do {
+                int boardDimension;
+                char activePlayer = 'X';
+                int i;
+                int j;
+
+                System.out.println("Please provide the board dimension (min 3, max 10): ");
+                boardDimension = scanner.nextInt();
+
+                    while (boardDimension < 3 || boardDimension > 10) {
+                    System.out.println("Incorrect data provided. Enter the board dimension again (min 3, max 10): ");
+                    boardDimension = scanner.nextInt();
+                    }
+
+                char[][] board = new char[boardDimension][boardDimension];
+
+                completeTheBoard(board);
                 printTheBoard(board);
 
-                if (activePlayer == 'X') {
-                    activePlayer = '0';
-                } else {
-                    activePlayer = 'X';
-                }
+                    while (gameResult(board, playersMap, mapOfWinningBoards) == '1') {
+                        System.out.println("Player " + activePlayer + ", give both coordinates: ");
+                        i = scanner.nextInt();
+                        j = scanner.nextInt();
+
+                        while (checkCoordinates(board, i, j)) {
+                            System.out.println("Values too big. Enter max value " + (board.length-1) + ": ");
+                            i = scanner.nextInt();
+                            j = scanner.nextInt();
+                        }
+
+                        while (!updateTheBoard(board, i, j, activePlayer)) {
+                            System.out.println("Field occupied. Enter other coordinates: ");
+                            i = scanner.nextInt();
+                            j = scanner.nextInt();
+                        }
+
+                        printTheBoard(board);
+
+                        if (activePlayer == 'X') {
+                            activePlayer = '0';
+                        } else {
+                            activePlayer = 'X';
+                        }
+                    }
+                System.out.println("Shall we play again? Enter YES or NO.");
+                scanner.nextLine();
+                continueOrGameOver = scanner.nextLine();
             }
+            while (continueOrGameOver.equalsIgnoreCase("YES"));
+                System.out.println("Results:\n" +
+                                   playersMap.get('X') + " : " + mapOfWinningBoards.get(playersMap.get('X')) + " board won\n" +
+                                   playersMap.get('0') + " : " + mapOfWinningBoards.get(playersMap.get('0')) + " board won\n");
+                System.out.println("Thank you for the game!");
         }
     }
 
@@ -80,7 +119,10 @@ public class Main {
         }
     }
 
-    private static boolean updateTheBoard(char[][] board, int i, int j, char activePlayer) {
+    private static boolean updateTheBoard(char[][] board,
+                                          int i,
+                                          int j,
+                                          char activePlayer) {
 
         if (board[i][j] == '-') {
             board[i][j] = activePlayer;
@@ -95,8 +137,12 @@ public class Main {
         char row = ' ';
 
         for (int i = 0; i < board.length; i++) {
-            if ((board[i][0] == board[i][1]) && board[i][1] == board[i][2] && board[i][0] != '-') {
+            if (board[i][0] != '-') {
                 row = board[i][0];
+                row = checkLines(board, i, row, 'r');
+            }
+            if (row != ' ') {
+                return row;
             }
         }
         return row;
@@ -106,44 +152,69 @@ public class Main {
 
         char col = ' ';
 
-        for (int i = 0; i < board[0].length; i++) {
-            if ((board[0][i] == board[1][i]) && board[1][i] == board[2][i] && board[0][i] != '-') {
+        for (int i = 0; i < board.length; i++) {
+            if (board[0][i] != '-') {
                 col = board[0][i];
+                col = checkLines(board, i, col, 'c');
+            }
+            if (col != ' ') {
+                return col;
             }
         }
         return col;
     }
 
+    private static char checkLines(char[][] board,
+                                   int i,
+                                   char oneCharacterInLine,
+                                   char type) {
+
+                for (int j = 1; j < board[i].length; j++) {
+                    if (type == 'c') {
+                        if (board[j][i] != oneCharacterInLine) {
+                            oneCharacterInLine = ' ';
+                        }
+                    } else {
+                        if (board[i][j] != oneCharacterInLine) {
+                            oneCharacterInLine = ' ';
+                        }
+                    }
+                }
+                return oneCharacterInLine;
+    }
+
     private static char checkDiagonals(char[][] board) {
 
+        int w = 0;
         int x = 0;
         int y = 0;
         int z = 0;
-        int g = 0;
 
         for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 'X') {
-                x++;
+                w++;
             } else if (board[i][i] == '0') {
-                y++;
+                x++;
             }
             if (board[i][board.length-i-1] == 'X') {
-                z++;
+                y++;
             } else if (board[i][board.length-i-1] == '0') {
-                g++;
+                z++;
             }
         }
 
-        if (x == 3 || z == 3) {
+        if (w == board.length || y == board.length) {
             return 'X';
-        } else if (y == 3 || g == 3) {
+        } else if (x == board.length || z == board.length) {
             return '0';
         } else {
             return ' ';
         }
     }
 
-    private static char gameResult(char[][] board) {
+    private static char gameResult(char[][] board,
+                                   Map<Character, String> playersMap,
+                                   Map<String, Integer> mapOfWinningBoards) {
 
         char result;
 
@@ -151,6 +222,8 @@ public class Main {
 
         if (result != ' ') {
             System.out.println("The winner is the player: " + result + ". Congratulations!");
+            mapOfWinningBoards.replace(playersMap.get(result),
+                                       mapOfWinningBoards.get(playersMap.get(result))+1);
             return result;
         }
 
@@ -158,6 +231,8 @@ public class Main {
 
         if (result != ' ') {
             System.out.println("The winner is the player: " + result + ". Congratulations!");
+            mapOfWinningBoards.replace(playersMap.get(result),
+                                       mapOfWinningBoards.get(playersMap.get(result))+1);
             return result;
         }
 
@@ -165,13 +240,15 @@ public class Main {
 
         if (result != ' ') {
             System.out.println("The winner is the player: " + result + ". Congratulations!");
+            mapOfWinningBoards.replace(playersMap.get(result),
+                                       mapOfWinningBoards.get(playersMap.get(result))+1);
             return result;
         }
 
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++){
-                if (board[i][j] == '-'){
-                    result = '1';
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '-') {
+                    result = '1'; // '1' means the game continues
                 }
             }
         }
